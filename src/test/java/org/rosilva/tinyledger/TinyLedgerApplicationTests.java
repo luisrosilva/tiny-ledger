73aada6bfffdb9693800c6738c0902339b01b5d9
luisrosilva <silva.luis@gmail.com>
2025-08-01 17:56:25 +0200

    Initial project scaffolding. Initial api for (very) basic account management.

---
+++ src/test/java/org/rosilva/tinyledger/TinyLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rosilva.tinyledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
